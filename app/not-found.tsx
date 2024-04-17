21a056a1be3855da53bb11aeb63ed830b45bd125
Markus Wedler <markuswedler8@gmail.com>
2024-04-17 22:03:34 +1000

    Add not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <div className="h-full grid place-content-center gap-2">
+      <div className="flex items-center">
+        <div className="text-[1.5rem] font-medium leading-[3.0625rem] border-r-[0.0625rem] border-[#ffffff4d] pr-[1.4375rem] mr-[1.25rem]">
+          404
+        </div>
+        <p className="text-[.875rem]">This page could not be found.</p>
+      </div>
+    </div>
+  )
+}
